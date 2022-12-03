ca0aba382bea0eebdae1897cf446c79dd2f65d3c
Emir Balaban <95624033+Em1r-B61@users.noreply.github.com>
2022-12-03 23:39:59 +0300

    Add files via upload

---
+++ Örnek_1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class program
+{
+    public static void Main()
+    {
+        int a = 2; int b = 3; int c = 6; int d = 1;
+
+        Console.WriteLine("(a<b)&&(c<d) --> " + ((a < b) && (c < d)));
+        Console.WriteLine("(a<b)||(c<d) --> " + ((a < b) || (c < d)));
+        Console.WriteLine("! (a<b) --> " + (!(a < b)));
+        Console.WriteLine("(a<b)&(c<d) --> " + ((a < b) & (c < d)));
+        Console.WriteLine("(a<b)|(c<d) --> " + ((a < b) | (c < d)));
+        Console.WriteLine("(a<b)^(c<d) --> " + ((a < b) ^ (c < d)));
+    }
+}
